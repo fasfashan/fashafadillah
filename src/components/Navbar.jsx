82e7472d164ba84c73e05198578f6bb85cd1f110
fasfashann <saintgloria.jkt@gmail.com>
2023-11-05 16:27:46 +0700

    add link for logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 export default function Navbar() {
   return (
     <nav className="px-5 py-4  mt-3 border rounded-lg flex items-center justify-between bg-secondary border-gray ">
-      <h1 className="md:text-3xl text-2xl font-bold">Fasha.</h1>
+      <a
+        href="/"
+        className="md:text-3xl hover:text-neutral-400 text-2xl font-bold"
+      >
+        Fasha.
+      </a>
       <ul className="flex items-center gap-6 text-sm">
         <li className="hover:text-neutral-400 transition-all">
           <a href="#work">Work</a>
